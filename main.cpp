f358382ab7c02d0e28b8838ac1f534c9144b05e6
Harddrive <kolchinalexandr@mail.com>
2021-06-24 22:40:51 +0300

    Added gitignore reconfigured project

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "matrix.h"
 
 int main(int, char**) {
     std::cout << "Hello, world!\n";
---
+++ matrix.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Matrix 
+{
+public:
+    Matrix();
+    ~Matrix();
+
+private:
+    int m_num_rows;
+    int m_num_cols;
+};
